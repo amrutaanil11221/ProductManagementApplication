1b5bc6ea8a1e98db82948e65a4b71d4c5b8088d3
amrutaanil11221 <155294617+amrutaanil11221@users.noreply.github.com>
2024-07-12 15:05:12 +0530

    Delete ProductApi directory

--- ProductApi/Model/RequestModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DemoProject.Model
-{
-    public class RequestModel
-    {
-        public string Name { get; set; }
-        public float Price { get; set; }
-        public int Quantity { get; set; }
-    }
-}
